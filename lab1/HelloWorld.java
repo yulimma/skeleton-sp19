1e233170f039bb9dc4171257822efd9a0764df00
yulimma <myl2014kobe@gmail.com>
2019-05-29 21:19:12 +0800

    Add learning comments to HelloWorld.java

--- lab1/HelloWorld.java
+++ lab1/HelloWorld.java
@@ -3,3 +3,10 @@ public class HelloWorld{
 	System.out.println("Hello World");
 	}
 }
+
+/*
+1. All code in java must be part of a class.
+2. We delimit the begining and end of segments of code using { and }.
+3. All segments in java must end in a semi-colon.
+4. For code to run we need punlic static void main(String[] args).
+*/
